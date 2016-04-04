827e325e5dabeaadb7a58cb927e42f6d6e253339
William Liddy <wjl36@case.edu>
2016-04-03 21:02:48 -0400

    Nav Mesh Generator

--- AD2Editor.cs
+++ AD2Editor.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 
 public class AD2Editor : AD2Game
@@ -11,7 +12,25 @@ public class AD2Editor : AD2Game
         public string name;
         public Texture2D t;
         public Texture2D collide;
-        public int X, Y;
+        public int X, Y;                
+    }
+
+    //RangedPathFinding node
+    public class RPFNode
+    {
+        public RPFNode(int startX, int startY, int ID)
+        {
+            this.ID = ID;
+            frontier = new LinkedList<int[]>();
+            nodesInSet = new LinkedList<int[]>();
+            frontier.AddFirst(new int[2] { startX, startY });
+            nodesInSet.AddFirst(new int[2] { startX, startY });
+            edges = new LinkedList<int>();
+        }
+        public int ID;
+        public LinkedList<int[]> frontier;
+        public LinkedList<int[]> nodesInSet;
+        public LinkedList<int> edges;
     }
 
     //top left corner
@@ -19,6 +38,10 @@ public class AD2Editor : AD2Game
     int camY = 0;
     int mouseX = 0;
     int mouseY = 0;
+
+
+    int MaxCharacterHeight = 16;
+    int RangedPathFindingNodeDistance = 14;
 
     public LinkedList<AD2Object>[] objectsList;
 
@@ -141,7 +164,7 @@ public class AD2Editor : AD2Game
     protected override void AD2Draw(AD2SpriteBatch primarySpriteBatch)
     {
         primarySpriteBatch.DrawTexture(baseMap, -camX, -camY);
-        Utils.Log(mouseX + "");
+
         for(int y = 0; y != objectsList.Length;y++)
         {
             foreach(AD2Object a in objectsList[y])
@@ -190,7 +213,8 @@ public class AD2Editor : AD2Game
 
     public bool collide(int x1, int y1, int width, int height, int pointX, int pointY)
     {
-        return (pointX >= x1 && pointY >= y1 && pointX <= x1 + width && pointY <= y1 + height);
+
+        return (pointX >= x1 && pointY >= y1 && pointX <= x1 + (width - 1) && pointY <= y1 + (height - 1));
     }
 
     public void generateObjectsXML()
@@ -245,7 +269,176 @@ public class AD2Editor : AD2Game
         stream.Dispose();
 
 
+        generatePathfindingMesh(map);
 
     }
+    
+    // This is probably the biggest for-tower i've ever created and it does a LOT of repeat calcuations.
+    public void generatePathfindingMesh(RenderTarget2D collmap)
+    {
+        Color[] pixels = new Color[collmap.Width * collmap.Height];
+        collmap.GetData(pixels);
+        //All of the places we can walk.
+        bool[,] notWalkable = new bool[baseMap.Width, baseMap.Height];
+        
+        for (int x = 0; x != collmap.Width; x++)
+        {
+            for(int y = 0; y != collmap.Height; y++)
+            {
+                //BAD: Assumes 255/0/255 magic pank
+                if(pixels[x + y  *collmap.Width].R == 255 && pixels[x + y * collmap.Width].G == 0 && pixels[x + y * collmap.Width].B == 255)
+                {
+                    for (int height = MaxCharacterHeight; height >= 0; height--)
+                    {
+                        for (int width = MaxCharacterHeight; width >= 0; width--)
+                        {
+                            if (y - height < 0 || x - width < 0)
+                                    continue;
+                            notWalkable[x - width, y - height] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        //Cannot walk along the far sides of map either.
+        for (int x = 0; x != baseMap.Width; x++)
+        {
+            for(int y = 0; y != baseMap.Height; y++)
+            {
+                if (x > baseMap.Width - MaxCharacterHeight || y > baseMap.Height - MaxCharacterHeight)
+                {
+                    notWalkable[x, y] = true;
+                }
+            }
+        }
+
+        //step two: Generate a List of pathfinding node origins.
+        bool[,] nodeClaimed = new bool[baseMap.Width, baseMap.Height];
+        int[,] nodeOwner = new int[baseMap.Width, baseMap.Height];
+
+        LinkedList<RPFNode> seeds = new LinkedList<RPFNode>();
+        Utils.Log("Generating Seeds...");
+        int nextMeshRegionID = 0;
+
+        for (int xStart = 0; xStart < baseMap.Width; xStart += RangedPathFindingNodeDistance)
+        {
+            for(int yStart = 0; yStart < baseMap.Height; yStart+= RangedPathFindingNodeDistance)
+            {
+                if (!notWalkable[xStart, yStart])
+                {
+                    nodeClaimed[xStart, yStart] = true;
+                    RPFNode newNode = new RPFNode(xStart, yStart, nextMeshRegionID);
+                    seeds.AddFirst(newNode);
+                    nextMeshRegionID++;
+                }
+            }
+        }
+
+
+        Utils.Log("Growing Seeds...");
+        LinkedList<RPFNode> rootRegions = new LinkedList<RPFNode>();
+        //step 3: Grow nodes until they can grow no longer. 
+        while (seeds.Count > 0)
+        {
+            foreach (RPFNode seed in seeds)
+            {
+                //grow frontiers
+                LinkedList<int[]> oldFrontier = seed.frontier;
+                seed.frontier = new LinkedList<int[]>();
+                foreach (int[] coord in oldFrontier)
+                {
+                    LinkedList<int[]> dirs = new LinkedList<int[]>();
+                    dirs.AddFirst(new int[2] { -1, 0 });
+                    dirs.AddFirst(new int[2] { 1, 0 });
+                    dirs.AddFirst(new int[2] { 0, 1 });
+                    dirs.AddFirst(new int[2] { 0, -1 });
+                    foreach (int[] d in dirs)
+                    {
+                        int dx = d[0];
+                        int dy = d[1];
+                        if (collide(0, 0, baseMap.Width, baseMap.Height, coord[0] + dx, coord[1] + dy) && !nodeClaimed[coord[0] + dx, coord[1] + dy] && !notWalkable[coord[0] + dx, coord[1] + dy])
+                        {
+                            int[] newcoord = new int[2] { coord[0] + dx, coord[1] + dy };
+                            seed.frontier.AddFirst(newcoord);
+                            seed.nodesInSet.AddFirst(newcoord);
+                            nodeClaimed[newcoord[0], newcoord[1]] = true;
+                            nodeOwner[newcoord[0], newcoord[1]] = seed.ID;
+
+                        }
+                    }
+                }
+            }
+
+            //remove nodes who have no frontier to grow.
+            LinkedList<RPFNode> toRemove = new LinkedList<RPFNode>();
+            foreach (RPFNode seed in seeds)
+            {
+                if (seed.frontier.Count == 0)
+                    toRemove.AddFirst(seed);
+            }
+
+            foreach (RPFNode n in toRemove)
+            {
+                seeds.Remove(n);
+                rootRegions.AddFirst(n);
+            }
+        }
+
+
+        //step 4: find neighbors.
+        Utils.Log("Finding Neighbors...");
+        foreach (RPFNode region in rootRegions)
+        {
+            foreach (int[] node in region.nodesInSet)
+            {
+                //look for neigthbors.
+                LinkedList<int[]> dirs = new LinkedList<int[]>();
+                dirs.AddFirst(new int[2] { -1, 0 });
+                dirs.AddFirst(new int[2] { 1, 0 });
+                dirs.AddFirst(new int[2] { 0, 1 });
+                dirs.AddFirst(new int[2] { 0, -1 });
+                foreach (int[] d in dirs)
+                {
+                    int dx = d[0];
+                    int dy = d[1];
+                    if (collide(0, 0, baseMap.Width, baseMap.Height, node[0] + dx, node[1] + dy))
+                    {
+                        if (nodeOwner[node[0] + dx, node[1] + dy] != region.ID)
+                        {
+                            //We have an edge.
+                            if (!region.edges.Contains(nodeOwner[node[0] + dx, node[1] + dy]))
+                                region.edges.AddFirst(nodeOwner[node[0] + dx, node[1] + dy]);
+                        }
+                    }
+                }
+            }
+        }
+
+        //Step 5: The graph is complete, we have regions that can reach other regions. Write to XML.
+        //create a new file 
+        Utils.Log("Printing...");
+        string path = "PathFindingMesh.xml";
+        File.Create(path).Close();
+        StreamWriter tw = new StreamWriter(path);
+
+        tw.WriteLine("<PathMesh>");
+        foreach (RPFNode node in rootRegions)
+        {
+            foreach (int neighbor in node.edges)
+            {
+                tw.WriteLine("    <Region" + node.ID + "Edge>" + neighbor + "</Region" + node.ID + "Edge>" );
+            }
+            foreach (int[] pixel in node.nodesInSet)
+            {
+                tw.WriteLine("    <Region" + node.ID + "Pixel>" + pixel[0] +"," + pixel[1] + "</Region" + node.ID + "Pixel>");
+            }
+        }
+        tw.WriteLine("    <MeshRegionCount>" + nextMeshRegionID + " </MeshRegionCount>");
+        tw.WriteLine("</PathMesh>");
+        tw.Close();
+    }
+
+
 }
 
